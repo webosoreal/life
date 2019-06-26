c2cdbf0d671b9e9a165a79a498e06ce613b00b50
Antonio Sannino - External <info@weboso.it>
2019-06-26 15:29:21 +0200

    go on firebase

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -3,22 +3,94 @@ import Game from '../js/game.js'
 class GameList {
 	constructor(database) {
 		this.database = database
+		this.setEvents()
 	}
 
 	newGame() {
-		this.games[this.games.length] = new Game()
+		var tabNew = document.querySelector('.signUp')
+		tabNew.classList.remove('hidden')
+		var button = tabNew.querySelector('button')
+
+		button.addEventListener('click', ()=> {
+			var mail = document.querySelector('.signUp input[name="mail"]').value
+			var pass = document.querySelector('.signUp input[name="password"]').value
+
+			firebase.auth().createUserWithEmailAndPassword(mail, pass).catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log('errorSignUp', errorCode, errorMessage)
+				alert(errorMessage)
+			});
+
+			setTimeout(() => {
+				firebase.auth().signInWithEmailAndPassword(mail, pass).catch(function(error) {
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					console.log('errorSignIn', errorCode, errorMessage)
+					alert(errorMessage)
+				});
+			}, 1000)
+		})
+	}
+
+	loadGame() {
+		var tabLoad = document.querySelector('.loginGame')
+		tabLoad.classList.remove('hidden')
+		var button = tabLoad.querySelector('button')
+
+		button.addEventListener('click', ()=> {
+			var mail = document.querySelector('.loginGame input[name="mail"]').value
+			var pass = document.querySelector('.loginGame input[name="password"]').value
+
+			firebase.auth().signInWithEmailAndPassword(mail, pass).catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log('errorSignIn', errorCode, errorMessage)
+			});
+		});
 	}
 
 	setEvents() {
-		document.getElementById('newGame').addEventListener('click', () => {
+		document.getElementById('createGame').addEventListener('click', () => {
 			this.newGame()
+		})
+
+		document.getElementById('loadGame').addEventListener('click', () => {
+			this.loadGame()
+		})
+
+		firebase.auth().onAuthStateChanged(function(user) {
+			if(user) {
+				var uid = user.uid
+				var ref = firebase.database().ref(uid);
+
+				ref.on("value", function(snapshot) {
+					var res = snapshot.val()
+					if(res && res.game == 'started') {
+						console.log('game already started')
+
+						database.ref('game').set({
+							'game' : 'started'
+						})
+					} else {
+						database.ref().set({
+							uid : {},
+						});
+
+						database.ref(uid).set({
+							'game' : 'started'
+						})
+
+						console.log('start game')
+						new Game()
+					}
+				}, function (error) {
+					console.log("Error: " + error.code);
+				});
+			}
 		})
 	}
 }
-
-window.gameList = new GameList()
-window.gameList.users = [{}]
-window.gameList.users['ciupa'] = {'pippo': 'uno'}
 
 var firebaseConfig = {
 	apiKey: "AIzaSyDvUF3eaH10JQybKYYRS2NsaJiKzEt_xJg",
@@ -31,27 +103,5 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-var database = firebase.database().ref('users')
-
-// firebase.auth().createUserWithEmailAndPassword('mail@mail.it', 'password').catch(function(error) {
-// 	var errorCode = error.code;
-// 	var errorMessage = error.message;
-// 	console.log('errorSignUp', errorCode, errorMessage)
-// });
-
-firebase.auth().signInWithEmailAndPassword('mail@mail.it', 'password').catch(function(error) {
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	console.log('errorSignIn', errorCode, errorMessage)
-});
-
-firebase.auth().onAuthStateChanged(function(user) {
-	console.log(user) //user.uid
-})
-
-// database.set({
-//     'gameList': window.gameList
-// });
-
-// new GameList(database)
-
+var database = firebase.database()
+new GameList(database)
