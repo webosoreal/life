b0461bf7afd0f94e887dafd5824d300df1279f08
Antonio Sannino - External <info@weboso.it>
2019-06-26 12:04:57 +0200

    try to init firebase, rework all

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,10 +1,8 @@
 import Game from '../js/game.js'
 
 class GameList {
-	constructor() {
-		this.games = [] // fetch games from server
-
-		this.setEvents()
+	constructor(database) {
+		this.database = database
 	}
 
 	newGame() {
@@ -19,3 +17,41 @@ class GameList {
 }
 
 window.gameList = new GameList()
+window.gameList.users = [{}]
+window.gameList.users['ciupa'] = {'pippo': 'uno'}
+
+var firebaseConfig = {
+	apiKey: "AIzaSyDvUF3eaH10JQybKYYRS2NsaJiKzEt_xJg",
+	authDomain: "life-b949f.firebaseapp.com",
+	databaseURL: "https://life-b949f.firebaseio.com",
+	projectId: "life-b949f",
+	storageBucket: "",
+	messagingSenderId: "406263429423",
+	appId: "1:406263429423:web:4771258a20d1f07e"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database().ref('users')
+
+// firebase.auth().createUserWithEmailAndPassword('mail@mail.it', 'password').catch(function(error) {
+// 	var errorCode = error.code;
+// 	var errorMessage = error.message;
+// 	console.log('errorSignUp', errorCode, errorMessage)
+// });
+
+firebase.auth().signInWithEmailAndPassword('mail@mail.it', 'password').catch(function(error) {
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	console.log('errorSignIn', errorCode, errorMessage)
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+	console.log(user) //user.uid
+})
+
+// database.set({
+//     'gameList': window.gameList
+// });
+
+// new GameList(database)
+
